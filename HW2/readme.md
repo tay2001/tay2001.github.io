3a419d6f0a86894cf8f3a786ffcd65086546a2a1
Taylor Powell <71796179+tay2001@users.noreply.github.com>
2023-01-25 09:51:04 -0700

    Add files via upload

---
+++ HW2/readme.md
@@ -0,0 +1,5 @@
+MART 441 - HW 2
+This week was pretty light, and I had no issue completing it. I have implemented many JS scripts before on other
+web projects like an ECommerce store, Django projects, etc.. Overall, implementing and getting a quick refresher
+on implementing a script and thinking of the cultural importance of my favorite artists was a nice change of pace
+and I enjoyed this week. I am excited to get into more complex JS.
---
+++ HW2/script.js
@@ -0,0 +1,10 @@
+console.log("Favorite Websites:");
+console.log("Amazon, Youtube, Netflix");
+
+console.log("Favorite Games");
+console.log("Monster Hunter, Pokemon, Overwatch");
+
+console.log("Favorite Artists");
+console.log("Vivaldi - He perfected the classical three-movement concerto.");
+console.log("Tchaikovsky - Merged drama and emotion with the symphonic form. ");
+console.log("Mendelssohn - Integrated his original pieces with elements from the Classical and Romantic music to form a unique style.");
